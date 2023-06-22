697856f41088a948e77625a84c5f2168a3bb690b
Poodoop <55837575+Poodoop@users.noreply.github.com>
2023-06-22 17:07:23 +0700

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,9 +1,4 @@
-#jalankan 5 async_server
-
-python3 async_server.py 9002 &
-python3 async_server.py 9003 &
-python3 async_server.py 9004 &
-python3 async_server.py 9005 &
-
-
-
+python3 async_server.py 8001 &
+python3 async_server.py 8002 &
+python3 async_server.py 8003 &
+python3 async_server.py 8004 &
